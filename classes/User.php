c79f716876fc3aa21a06b16df462a8c519d31182
Akane-F <akanefukuda.26@gmail.com>
2025-08-27 15:28:45 -0700

    Add User class

--- classes/User.php
+++ classes/User.php
@@ -1,5 +1,63 @@
 <?php
 
-class User {
-    
+class User
+{
+    private string $name;
+    private string $email;
+    private string $password;
+    private string $role;
+
+    public function __construct(string $name, string $email, string $password, string $role = 'user')
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $this->hashPassword($password);
+        $this->role = $role;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email)
+    {
+        $this->email = $email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password)
+    {
+        $this->name = $password;
+    }
+
+    private function hashPassword(string $plainPassword): string
+    {
+        $peppered = hash_hmac("sha256", $plainPassword, PEPPER);
+        return password_hash($peppered, PASSWORD_BCRYPT, ['cost' => 11]);
+    }
+
+    public function getRole(): string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role)
+    {
+        $this->role = $role;
+    }
 }
