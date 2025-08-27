9182635e2ee0171fa3fd98ab9eb57b087648cd67
Akane-F <akanefukuda.26@gmail.com>
2025-08-27 15:29:11 -0700

    Add create User function

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,6 +1,9 @@
 <?php
-
 namespace Controllers;
+
+require_once __DIR__. '/../classes/User.php';
+
+use User;
 
 class UserController
 {
@@ -13,7 +16,26 @@ class UserController
 
     public function create()
     {
-        echo "create user";
+        // Validate _POST
+        print_r($_POST);
+        if (!isset($_POST['name']) || !isset($_POST['email']) || !isset($_POST['password'])) {
+            http_response_code(400);
+            echo 'Missing required fields';
+            return;
+        }
 
+        $user = new User($_POST['name'], $_POST['email'], $_POST['password']);
+
+        $insertPrep = $this->db_conn->prepare("INSERT INTO Users (name, email, password) VALUES (?, ?, ?)");
+        $insertPrep->bind_param("sss",
+            $user->getName(),
+            $user->getEmail(),
+            $user->getPassword()
+        );
+        $insertPrep->execute();
+        $insertPrep->close();
+
+        http_response_code(201);
+        echo 'User created';
     }
 }
