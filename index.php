447aca87d3711712c93a2e04fbbe7c2173d20891
Cassio Figueiredo <cassio.figueiredof@gmail.com>
2025-08-27 20:03:05 -0700

    fix: Path

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/repositories/UserRepository.php';
-require_once __DIR__ . '/Controllers/UserController.php';
+require_once __DIR__ . '/controllers/UserController.php';
 require_once __DIR__ . '/enums/UserRole.php';
 require_once __DIR__ . '/controllers/ProductController.php';
 require_once __DIR__ . '/repositories/ProductRepository.php';
